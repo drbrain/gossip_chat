9ee2d1cfcb1283a30c992b5d5b02729f052a7307
Eric Hodel <drbrain@segment7.net>
2014-04-22 20:54:49 -0700

    Add multicast address exchange to GossipChat
    This is a start of work on infectious spread of information

--- lib/gossip_chat.rb
+++ lib/gossip_chat.rb
@@ -1,5 +1,49 @@
 class GossipChat
   VERSION = '1.0'
+
+  def initialize
+    @announce    = GossipChat::Announce.new
+    @db          = {}
+    @lru         = GossipChat::LRU.new
+    @peers       = {}
+    @peers_mutex = Mutex.new
+  end
+
+  def broadcast_addresses
+    Thread.new do
+      loop do
+        @announce.broadcast_addresses
+        sleep 30 + rand(30)
+      end
+    end
+  end
+
+  def listen_for_peers
+    @announce.listen_for_addresses
+
+    Thread.new do
+      loop do
+        address = @announce.address_queue.pop
+        @peers_mutex.synchronize do
+          @peers[address] = true
+        end
+      end
+    end
+  end
+
+  def run
+    listen_for_peers
+    broadcast_addresses
+
+    loop do
+      sleep 10
+      @peers_mutex.synchronize do
+        puts 'addresses'
+        puts @peers.keys
+      end
+    end
+  end
+
 end
 
 require 'gossip_chat/announce'
